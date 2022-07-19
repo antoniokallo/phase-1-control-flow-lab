e31d8f769c36c2f23833c4886fb9db756cf7c0f5
antoniokallo <antoniokallo@gmail.com>
2022-07-19 11:53:04 -0400

    commit tree dea995bb939391060a6feeeb44fc4a894b2d6e93

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue) {
+   
+  if (someValue < 200){
+    return 'This one is on me!'
+  }
+  else if (someValue > 200 && someValue < 2000){
+    return 'That will be twenty bucks.'
+  }
+  else if (someValue > 2000 && someValue <=2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else (someValue > 2500)
+  {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someValue){
+if(someValue == 'NYC'){
+  return 'Ok, sounds good.'
+}
+else(someValue != 'NYC')
+{
+return 'No go.'
+}
 }
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+if (tip == 'generous'){
+  return 'Thank you so much.'
+}
+else if(tip == 'not as generous'){
+return 'Thank you.'
+}
+else (tip == 'thanks for everything')
+{
+return "Bye."
+}
 }
